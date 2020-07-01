dc0fc82218046ebe14a00f0153d1dbbb7644b4bc
OneMoreGame <belan.work@yahoo.com>
2020-07-01 18:57:02 +0300

    changed dir

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function (req, res) {
   res.header('Access-Control-Allow-Headers', 'content-type');
   res.header('Access-Control-Allow-Methods', 'GET');
   const fs = require('fs');
-  fs.readFile('D:/JS/dashboardBackend/result.txt', 'utf8', (err, contents) => {
+  fs.readFile('D:/JS/trendingAPI/python/result/result.txt', 'utf8', (err, contents) => {
     res.set("Content-Type", "application/json");
     res.send(contents)
   })
@@ -15,7 +15,7 @@ router.get('/', function (req, res) {
 
 setInterval(() => {
   const spawn = require("child_process").spawn;
-  const process = spawn('python', ["D:/JS/dashboardBackend/routes/script.py"]);
+  const process = spawn('python', ["D:/JS/trendingAPI/python/script.py"]);
   process.stdout.on('data', function (data) {
     const date = new Date()
     const dateString = `Updated ${date.getHours()}:${date.getMinutes()}`
