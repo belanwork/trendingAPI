6b6bb09bc31e6c31a339a8fa2ad9cc2341035cf8
OneMoreGame <belan.work@yahoo.com>
2020-07-01 19:02:01 +0300

    changed dir for linux

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function (req, res) {
   res.header('Access-Control-Allow-Headers', 'content-type');
   res.header('Access-Control-Allow-Methods', 'GET');
   const fs = require('fs');
-  fs.readFile('D:/JS/trendingAPI/python/result/result.txt', 'utf8', (err, contents) => {
+  fs.readFile('/root/trendingAPI/python/result/result.txt', 'utf8', (err, contents) => {
     res.set("Content-Type", "application/json");
     res.send(contents)
   })
@@ -15,7 +15,7 @@ router.get('/', function (req, res) {
 
 setInterval(() => {
   const spawn = require("child_process").spawn;
-  const process = spawn('python', ["D:/JS/trendingAPI/python/script.py"]);
+  const process = spawn('python', ["/root/trendingAPI/python/script.py"]);
   process.stdout.on('data', function (data) {
     const date = new Date()
     const dateString = `Updated ${date.getHours()}:${date.getMinutes()}`
@@ -24,3 +24,6 @@ setInterval(() => {
 }, 600000)
 
 module.exports = router;
+
+// fs.readFile('D:/JS/trendingAPI/python/result/result.txt', 'utf8', (err, contents) => {
+// const process = spawn('python', ["D:/JS/trendingAPI/python/script.py"]);
